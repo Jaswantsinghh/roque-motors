07b9e9263ca364acde5cee6317c3b91bb1c72752
Jaswantsinghh <jaswantsinghjsn@gmail.com>
2020-06-15 20:49:55 +0530

    fetch event added

--- sw.js
+++ sw.js
@@ -2,7 +2,13 @@
 self.addEventListener('install', evt => {
   console.log('service worker has been installed');
 });
+
 //activate service Worker
 self.addEventListener('activate', evt => {
   console.log('service worker has been activated');
+});
+
+//fetch events
+self.addEventListener('fetch', evt=> {
+  console.log('fetch event', evt);
 });
